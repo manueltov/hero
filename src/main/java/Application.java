c18558abef719e62217d718cfc365507da64821d
manueltov <manuel@tovar.pt>
2019-02-25 16:01:57 +0000

    Game classe added

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,25 +1,8 @@
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello everyone!!");
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);   // we don't need a cursor
-            screen.startScreen();             // screens must be started
-            screen.doResizeIfNecessary();     // resize screen if necessary
-
-            screen.clear();
-            screen.setCharacter(10, 10, new TextCharacter('X'));
-            screen.refresh();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        Game jogo = new Game();
+        jogo.run();
     }
 }
